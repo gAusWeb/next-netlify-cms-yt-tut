6c2a5a71e8c1f531ee624287cdfa40cce3f72b22
Gavin McQuienn <gavinmcquienn@gmail.com>
2021-06-26 00:02:47 +1000

    Testing netlify forms

--- pages/about.js
+++ pages/about.js
@@ -15,14 +15,25 @@ const About = () => {
                         <p><a href="./contact" title="Click to to go to home page">Contact</a></p>
                         <hr></hr>
                         <h1>About</h1>
-                        <div>
-                            <img src="https://picsum.photos/2500/600" className="img-fluid" alt="Responsive image"/>
-                        </div>
-                        
-                        <div>
-                            <img src="https://picsum.photos/2500/600" className="img-fluid" alt="Responsive image"/>
-                        </div>
+                        <hr></hr>
 
+
+                        <form name="contact" method="POST" data-netlify="true">
+                            <div className="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                            </div>
+                            <div className="form-group form-check">
+                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                            </form>
                     </div> 
                 </div> 
             </div> 
