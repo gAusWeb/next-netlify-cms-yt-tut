960f0a593be963e533ad2c84bc6181b7b06feda9
Gavin McQuienn <gavinmcquienn@gmail.com>
2021-07-13 22:39:21 +1000

    Search - trying to access mySearchIndex.json

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Layout from "../components/Layout"
 import "../fed/scss/quick-image-reset.css"
 import "../fed/scss/bootstrap-v4-4-1.css"
-import searchIndex from './searchIndex.json'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
