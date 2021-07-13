67ad1b293b6882fb0f9daf7ce430265c0b916a29
Gavin McQuienn <gavinmcquienn@gmail.com>
2021-07-13 22:31:05 +1000

    Search - generated mySearchIndex.json

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout"
 import "../fed/scss/quick-image-reset.css"
 import "../fed/scss/bootstrap-v4-4-1.css"
+import searchIndex from './searchIndex.json'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
