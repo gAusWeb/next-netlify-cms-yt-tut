6e7e79184ad9862ed82d3571c9eeabb0f57b68a8
Gavin McQuienn <gavinmcquienn@gmail.com>
2021-07-21 23:26:53 +1000

    BlogList - bootstrap containers fixed

--- components/BlogList.js
+++ components/BlogList.js
@@ -1,23 +1,32 @@
 const BlogList = ({ blogs, title, handleDelete }) => {
-    
     return (
         <>
             <div className="blog-list">
-                <hr />
-                <h1>{title}</h1>
-                <hr />
-                <div className="row">
-                    {blogs.map((blog, i) => (
-                        <div className="col" key={blog.id}>
-                            <div className="blog-preview">
-                                <h2>{blog.title}</h2>
-                                <p>Written by: {blog.author}</p>
-                                <button className="btn btn-danger" onClick={() => handleDelete(blog.id)}>
-                                    Delete Blog
-                                </button>
-                            </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h1>{title}</h1>
+                            <hr />
                         </div>
-                    ))}
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        {blogs.map((blog, i) => (
+                            <div className="col" key={blog.id}>
+                                <div className="blog-preview">
+                                    <h2>{blog.title}</h2>
+                                    <p>Written by: {blog.author}</p>
+                                    <button
+                                        className="btn btn-danger"
+                                        onClick={() => handleDelete(blog.id)}
+                                    >
+                                        Delete Blog
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
                 <br />
             </div>
