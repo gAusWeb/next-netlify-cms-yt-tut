3acce3f62a18cbfd6dd7044fe0e4b0d17b4a0583
gavAusWebAceik <gavinmcquienn@aceik.com.au>
2021-10-12 18:14:38 +1100

    Resolved non react references to 'class='

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -2,18 +2,18 @@ import React from "react";
 
 export default function HeroBanner(props) {
     return (
-        <section class="hero_banner">
-            <div class="hero_banner__bg-image"></div>
+        <section className="hero_banner">
+            <div className="hero_banner__bg-image"></div>
 
-            <div class="hero_banner__text-content">
-                <div class="hero_banner__text-content__container">
+            <div className="hero_banner__text-content">
+                <div className="hero_banner__text-content__container">
                     <div className="container">
                         <div className="row">
                             <div className="col">
-                                <h1 class="hero_banner__text-header">
+                                <h1 className="hero_banner__text-header">
                                     {props.heading}
                                 </h1>
-                                <h2 class="hero_banner__text-sub-header">
+                                <h2 className="hero_banner__text-sub-header">
                                     {props.subHeading}
                                 </h2>
                             </div>
